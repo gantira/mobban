986732eb4d1ef662a6622ae094f001049e1823c8
Gantira <angga.gantira@gmail.com>
2020-11-06 19:33:42 +0700

    add title categories

--- resources/views/livewire/categories/index.blade.php
+++ resources/views/livewire/categories/index.blade.php
@@ -35,6 +35,9 @@
         <!--begin::Header-->
         <div class="card-header flex-wrap border-0 pt-6 pb-0">
             <div class="card-title">
+                <h3 class="card-label">Data Table
+                    <span class="d-block text-muted pt-2 font-size-sm">Categories management made easy</span>
+                </h3>
             </div>
             <div class="card-toolbar">
                 <div class="input-icon input-icon-right">
