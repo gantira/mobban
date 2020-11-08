5aa6cf18fb3997476ab6c51a9b92fc7837ef1790
Gantira <angga.gantira@gmail.com>
2020-11-08 12:32:10 +0700

    fix dashboard

--- app/Http/Livewire/Dashboards/Index.php
+++ app/Http/Livewire/Dashboards/Index.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire\Dashboards;
 
+use App\Models\Category;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.dashboards.index');
+        return view('livewire.dashboards.index', [
+            'categoriesCount' => Category::all()
+        ]);
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function botsf()
+    {
+        return $this->hasMany(BotSf::class, 'kategori', 'name');
+    }
 }
