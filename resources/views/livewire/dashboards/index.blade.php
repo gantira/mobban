0e1b7400c385ccf96c64045e45d208cbc728c52f
Gantira <angga.gantira@gmail.com>
2020-12-02 09:46:04 +0700

    balikin dashboard

--- resources/views/livewire/dashboards/index.blade.php
+++ resources/views/livewire/dashboards/index.blade.php
@@ -32,17 +32,16 @@
         <!--begin::Header-->
         <div class="card-header flex-wrap border-0 pt-6 pb-0">
             <div class="card-toolbar">
-                <div class="mr-2">
-                    <select wire:model="datel" class="form-control" placeholder="Datel">
-                        <option value="">DATEL</option>
+                <div wire:ignore class="mr-2">
+                    <select wire:model="datel" class="form-control selectpicker datel" multiple="multiple" placeholder="Datel">
                         @foreach ($selectDatels as $item)
                             <option value="{{ $item->datel }}">{{ $item->datel }}</option>
                         @endforeach
                     </select>
                 </div>
-                <div>
-                    <select wire:model="sto" class="form-control"  placeholder="Datel">
-                        <option value="">STO</option>
+             
+                <div wire:ignore>
+                    <select wire:model="sto" class="form-control selectpicker sto" multiple="multiple" placeholder="Datel">
                         @foreach ($selectStos as $item)
                             <option value="{{ $item->sto }}">{{ $item->sto }}</option>
                         @endforeach
